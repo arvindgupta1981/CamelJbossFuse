3f973016b6258f194e0983db39cac3a3ebb67f32
arvind Gupta <arvind Gupta@192.168.1.4>
2015-11-29 14:17:40 +0530

    te

--- camel/Context/src/main/java/arv/rd/contextcomponent/CamelContext.java
+++ camel/Context/src/main/java/arv/rd/contextcomponent/CamelContext.java
@@ -6,6 +6,7 @@ public class CamelContext {
 	private static final org.apache.camel.CamelContext CONTEXT = new DefaultCamelContext();
 	
 	public static org.apache.camel.CamelContext getInstance() {
+		Components.addActiveMqComponent();
 		return CONTEXT;
 	}
 
--- camel/Context/src/main/java/arv/rd/contextcomponent/Components.java
+++ camel/Context/src/main/java/arv/rd/contextcomponent/Components.java
@@ -1,12 +1,18 @@
 package arv.rd.contextcomponent;
 
-import org.apache.activemq.camel.component.ActiveMQComponent;
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.Component;
+import org.apache.camel.component.jms.JmsComponent;
 
 public class Components {
+	private static final String BROKER_URL = "vm://localhost?broker.persistent=false";
+	
 	public static void addActiveMqComponent() {
-		Component activemq = ActiveMQComponent.activeMQComponent("vm://localhost?broker.persistent=false");
-		
-		CamelContext.getInstance().addComponent("activemq", activemq);
+		//Component activemq = ActiveMQComponent.activeMQComponent("vm://localhost?broker.persistent=false");
+		ConnectionFactory connectionFactoryActiveMq = new ActiveMQConnectionFactory(BROKER_URL);
+		Component jmsComponents = JmsComponent.jmsComponentAutoAcknowledge(connectionFactoryActiveMq);
+		CamelContext.getInstance().addComponent("jms", jmsComponents);
 	}
 }
